d42034fa79c2dc6aed05b10dd1a40f49dd18975e
grouazel <antoine.grouazel@ifremer.fr>
2023-11-14 15:28:59 +0100

    fix issue with version of the lib

--- cdsodatacli/__init__.py
+++ cdsodatacli/__init__.py
@@ -18,6 +18,11 @@ def get_version() -> str:
     except importlib_metadata.PackageNotFoundError:  # pragma: no cover
         return "unknown"
 
+try:
+    from importlib import metadata
+except ImportError: # for Python<3.8
+    import importlib_metadata as metadata
+__version__ = metadata.version('xsarslc')
 
 version: str = get_version()
-__version__ = get_version()
+# __version__ = get_version()
