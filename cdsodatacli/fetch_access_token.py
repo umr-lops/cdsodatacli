758d1cac6058df1fd4bf95c5bb6462d540cdabed
grouazel <antoine.grouazel@ifremer.fr>
2023-11-28 10:07:09 +0100

    add securities for token read/write functions

--- cdsodatacli/fetch_access_token.py
+++ cdsodatacli/fetch_access_token.py
@@ -8,7 +8,7 @@ import os
 import glob
 import random
 
-MAX_SESSION_PER_ACCOUNT = 4
+MAX_VALIDITY_ACCESS_TOKEN = 600  # sec (defined by CDS API)
 
 
 def get_bearer_access_token(quiet=True, specific_account=None):
@@ -21,8 +21,8 @@ def get_bearer_access_token(quiet=True, specific_account=None):
     """
     url_identity = conf["URL_identity"]
     if specific_account is None:
-        all_accounts = conf["logins"].keys()
-        login = all_accounts[0]
+        all_accounts = list(conf["logins"].keys())
+        login = random.choice(all_accounts)
         passwd = conf["logins"][all_accounts[0]]
     else:
         login = specific_account
@@ -36,7 +36,7 @@ def get_bearer_access_token(quiet=True, specific_account=None):
         prefix
         + " --location --request POST "
         + url_identity
-        + " --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode 'grant_type=password' --data-urlencode 'username=%s' --data-urlencode 'password=%s'  --data-urlencode 'client_id=cdse-public'"
+        + " --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode 'grant_type=password' --data-urlencode 'username=%s' --data-urlencode 'password=%s' --data-urlencode 'client_id=cdse-public'"
         % (login, passwd)
     )
 
@@ -47,11 +47,23 @@ def get_bearer_access_token(quiet=True, specific_account=None):
     toto = answer_identity.decode("utf8").replace("'", '"')
     data = json.loads(toto)
     if "access_token" not in data:
-        raise Exception('you probably have a bad account (%s) in your config file: %s',login,data)
-    return data["access_token"], date_generation_access_token
+        # raise Exception('you probably have a bad account (%s) in your config file: %s',login,data)
+        logging.info(
+            "you probably have a bad account (%s) in your config file: %s", login, data
+        )
+        token = None
+    else:
+        token = data["access_token"]
+
+    path_semphore_token = write_token_semphore_file(
+        login=login,
+        date_generation_access_token=date_generation_access_token,
+        token_dir=conf["token_directory"],access_token=token
+    )
+    return token, date_generation_access_token, login, path_semphore_token
 
 
-def write_token_semphore_file(safename, login, date_generation_access_token, token_dir):
+def write_token_semphore_file(login, date_generation_access_token, token_dir,access_token):
     """
 
     Parameters
@@ -67,57 +79,49 @@ def write_token_semphore_file(safename, login, date_generation_access_token, tok
     """
     path_semphore_token = os.path.join(
         token_dir,
-        "CDSE_access_token_%s_%s_%s.txt"
-        % (login, date_generation_access_token.strftime("%Y%m%dt%H%M%S"), safename),
+        "CDSE_access_token_%s_%s.txt"
+        % (login, date_generation_access_token.strftime("%Y%m%dt%H%M%S")),
     )
     fid = open(path_semphore_token, "w")
+    fid.write(access_token)
     fid.close()
     return path_semphore_token
 
 
-def get_list_of_exising_token(token_dir):
+def get_list_of_exising_token(token_dir, account=None):
     """
-    we suppose ech account CDSE have maximum 4 tokens
+
+    Parameters
+
+        account (str): optional
 
     Returns
     -------
         lst_token (list)
     """
-    lst_token = glob.glob(os.path.join(token_dir, "CDSE_access_token_*.txt"))
-    logging.debug("Number of token found: %s", len(lst_token))
+    if account is not None:
+        lst_token0 = glob.glob(
+            os.path.join(token_dir, "CDSE_access_token_%s_*.txt" % account)
+        )
+    else:
+        lst_token0 = glob.glob(os.path.join(token_dir, "CDSE_access_token_*.txt"))
+
     # nb_account = len(conf["logins"])
+    lst_token = []
+    for ll in lst_token0:
+        date_generation_access_token = datetime.datetime.strptime(
+            os.path.basename(ll).split("_")[4].replace('.txt',''), "%Y%m%dt%H%M%S"
+        )
+        if (
+            datetime.datetime.today() - date_generation_access_token
+        ).total_seconds() < MAX_VALIDITY_ACCESS_TOKEN:
+            lst_token.append(ll)
+    logging.debug("Number of token found: %s", len(lst_token))
     return lst_token
 
 
-def get_a_free_account(counts):
-    """
-
-    Parameters
-    ----------
-    counts (collections.defaultdict(int)) counter of active session for each CDSE account
-    Returns
-    -------
-
-    """
-    candidate = None
-    all_free_accounts = []
-    for acc in counts:
-        if counts[acc] < MAX_SESSION_PER_ACCOUNT:
-            all_free_accounts.append(acc)
-        else:
-            logging.debug("account: %s is full", acc)
-    logging.debug("counts after attribution %s", counts)
-    logging.debug("all_free_accounts %s", all_free_accounts)
-    if len(all_free_accounts) > 0:
-        candidate = random.choice(all_free_accounts)
-        counts[candidate] += 1
-    else:
-        logging.debug("there is no free CDSE account to use in your localconfig.yml")
-    return candidate, counts
-
-
 def remove_semaphore_token_file(
-    token_dir, safename, login, date_generation_access_token
+    token_dir, login, date_generation_access_token
 ):
     """
     this function is supposed to be used when a download is finished ( could be long time after the validity expired)
@@ -133,10 +137,12 @@ def remove_semaphore_token_file(
     """
     path_token = os.path.join(
         token_dir,
-        "CDSE_access_token_%s_%s_%s.txt"
-        % (login, date_generation_access_token.strftime("%Y%m%dt%H%M%S"), safename),
+        "CDSE_access_token_%s_%s.txt"
+        % (login, date_generation_access_token.strftime("%Y%m%dt%H%M%S")),
     )
-    os.remove(path_token)
-    logging.debug("token semaphore file removed")
-
-
+    exists = os.path.exists(path_token)
+    if exists and (
+        datetime.datetime.today() - date_generation_access_token
+    ).total_seconds() >= MAX_VALIDITY_ACCESS_TOKEN:
+        os.remove(path_token)
+        logging.debug("token semaphore file removed")
